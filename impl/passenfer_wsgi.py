f743f611c7fa389b9c53bd2e27962a24ba1f8672
wye <wenbo.ye@sony.com>
2020-05-11 01:12:43 -0700

    initial commit

--- impl/passenfer_wsgi.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-
-INTERP = "/home/cs411terabyte/python_test/bin/python"
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-
-from python_test import app as application
